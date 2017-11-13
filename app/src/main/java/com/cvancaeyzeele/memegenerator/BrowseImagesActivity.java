7d4a6d0da1cda1211b3bda2a09106f06838754f7
Courtney VanCaeyzeele <cvancaeyzeele@gmail.com>
2017-11-13 11:32:12 -0600

    Created new activity, renamed image files

---
+++ app/src/main/java/com/cvancaeyzeele/memegenerator/BrowseImagesActivity.java
@@ -0,0 +1,13 @@
+package com.cvancaeyzeele.memegenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowseImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_images);
+    }
+}
