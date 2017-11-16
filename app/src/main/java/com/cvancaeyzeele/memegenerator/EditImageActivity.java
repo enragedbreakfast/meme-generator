8a2b5ec18276a7b16faba98020ac243d948c0630
Courtney VanCaeyzeele <cvancaeyzeele@gmail.com>
2017-11-15 18:37:20 -0600

    Implemented check for WRITE_EXTERNAL_STORAGE permission

--- app/src/main/java/com/cvancaeyzeele/memegenerator/EditImageActivity.java
+++ app/src/main/java/com/cvancaeyzeele/memegenerator/EditImageActivity.java
@@ -1,6 +1,12 @@
 package com.cvancaeyzeele.memegenerator;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +20,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class EditImageActivity extends AppCompatActivity {
 
@@ -24,6 +36,8 @@ public class EditImageActivity extends AppCompatActivity {
     EditText txtImage2;
     String imgText2;
     TextView textView2;
+    ImageView image;
+    private final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +51,7 @@ public class EditImageActivity extends AppCompatActivity {
         int imageID = intent.getIntExtra("image", 0);
 
         View v = new ImageView(getBaseContext());
-        ImageView image = new ImageView(v.getContext());
+        image = new ImageView(v.getContext());
         image.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), imageID));
 
         // Set layout params for ImageView
@@ -133,5 +147,51 @@ public class EditImageActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void saveImage(View view) {
+        // Check for permission to save files
+        if (ContextCompat.checkSelfPermission(EditImageActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(EditImageActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else { // Permission has been granted
+            createBitmap();
+        }
+
+    }
+
+    public void createBitmap() {
+        // Get Bitmap of ImageView
+        BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        // Create the file
+        File sdCardDirectory = Environment.getExternalStorageDirectory();
+        File imageFile = new File(sdCardDirectory, System.currentTimeMillis() + ".png");
+
+        // Write the Bitmap
+        boolean success = false;
+
+        // Encode the file as a PNG image
+        FileOutputStream outStream;
+
+        try {
+            outStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream); // 100 keeps full quality
+            outStream.flush();
+            outStream.close();
+            success = true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (success) {
+            Toast.makeText(getApplicationContext(), "Image saved successfully", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Error saving image", Toast.LENGTH_LONG).show();
+        }
     }
 }
